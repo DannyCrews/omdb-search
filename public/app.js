62abcc704d577f5336b424c572f85e2fec96818e
dannycrews <dancrews@me.com>
2017-02-05 18:06:06 -0500

    Add Kickstart for styling and start writing some javascript

--- public/app.js
+++ public/app.js
@@ -1,6 +1,15 @@
+// wait until DO is loaded
 document.addEventListener("DOMContentLoaded", function() {
 
-  var title = document.querySelector("h1");
-  console.log(title);
+  //  Listen for form submit
+  document.querySelector('#search-form').addEventListener("submit", function(e){
+    e.preventDefault();
+    var title = document.getElementById("movie-title").value;
+    var BASE_URL = 'http://www.omdbapi.com/?';
+    var searchString = BASE_URL + title.split(' ').join('+');
+    console.log(searchString);
+  });
+
+  fetch('')
 
 });
