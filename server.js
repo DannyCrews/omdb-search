2035a70ff255506d845bccd2b2cd028bc256a460
dannycrews <dancrews@me.com>
2017-02-05 13:58:57 -0500

    Fix major server bugs and add comments to server.js

--- server.js
+++ server.js
@@ -1,25 +1,44 @@
-var express = require('express');
-var app = express();
 var fs = require('fs');
 var path = require('path');
+var express = require('express');
+// add require for body-parser
+var bodyParser = require('body-parser')
+// add morgan for request logging
+var logger = require('morgan')
 
-app.use(express.static(path.join(__dirname, '/public')));
+
+var app = express();
+
+// add logger to middleware
+app.use(logger("dev"));
+
+// set up serving of static assets
+// change 'join' to 'resolve' - make it crossplatform
+var publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+// add missing ')'
+// change 'use' to get and render index
+app.get('/', function(req, res){
+  res.end('index');
+});
 
 app.get('/favorites', function(req, res){
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
-;
+// add missing '})'
+});
 
 app.get('favorites', function(req, res){
   if(!req.body.name || !req.body.oid){
     res.send("Error");
     return
-  
+  // add closing '}'
+  }
+
   var data = JSON.parse(fs.readFileSync('./data.json'));
   data.push(req.body);
   fs.writeFile('./data.json', JSON.stringify(data));
@@ -27,6 +46,7 @@ app.get('favorites', function(req, res){
   res.send(data);
 });
 
-app.list(3000, function(){
+// correct misspell of 'listen'
+app.listen(3000, function(){
   console.log("Listening on port 3000");
 });
