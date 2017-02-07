7afbc737bace5362fbb568c2ed8ffe6ef60605e5
dannycrews <dancrews@me.com>
2017-02-07 11:04:00 -0500

    Get ombdapi search working and display results

--- public/app.js
+++ public/app.js
@@ -1,15 +1,41 @@
-// wait until DO is loaded
+// wait until DOM is loaded
 document.addEventListener("DOMContentLoaded", function() {
 
-  //  Listen for form submit
-  document.querySelector('#search-form').addEventListener("submit", function(e){
-    e.preventDefault();
+  //  Listen for search submittal
+  document.querySelector('#search-button').addEventListener("click", function(e){
     var title = document.getElementById("movie-title").value;
-    var BASE_URL = 'http://www.omdbapi.com/?';
+    var BASE_URL = 'http://www.omdbapi.com/?s=';
     var searchString = BASE_URL + title.split(' ').join('+');
-    console.log(searchString);
+
+    getMovie(searchString);
   });
 
-  fetch('')
+  function getMovie(searchString) {
+    var resultContainer = document.querySelector(".search-results");
+
+    fetch(searchString, {
+      method: 'get'
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(searchResp) {
+      document.querySelector("#movie-title").value = '';
+      resultContainer.innerHTML = '';
+
+      var results = searchResp.Search;
+      console.log(results);
+      for ( i = 0; i < results.length; i++) {
+        var titleParagraph = document.createElement("p");
+        var movie = document.createTextNode(results[i].Title);
+        titleParagraph.appendChild(movie);
+        console.log(titleParagraph);
+        resultContainer.appendChild(titleParagraph);
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+  }
 
 });
--- server.js
+++ server.js
@@ -2,30 +2,29 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 // add require for body-parser
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 // add morgan for request logging
-var logger = require('morgan')
-
+var logger = require('morgan');
 
 var app = express();
 
 // add logger to middleware
 app.use(logger("dev"));
-
 // set up serving of static assets
 // change 'join' to 'resolve' - make it crossplatform
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.set("views", path.resolve(__dirname, "views"))
-app.set("view engine", "ejs");
+app.get('/', express.static(path.join(__dirname, 'public')));
+// app.set("views", path.resolve(__dirname, "views"))
+// app.set("view engine", "ejs");
 
 // add missing ')'
 // change 'use' to get and render index
-app.get('/', function(req, res){
-  res.render("index");
-});
+// app.get('/', function(req, res){
+//   res.render("index");
+// });
 
 app.get('/favorites', function(req, res){
   var data = fs.readFileSync('./data.json');
